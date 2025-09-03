089a34ac94b648efc52edf3df519fca4e5341a4b
thalytamizukawa <mizukawathalyta@gmail.com>
2025-09-03 00:33:25 -0300

    Update servo_motor_code.ino

--- servo_motor_code.ino
+++ servo_motor_code.ino
@@ -1,50 +1,100 @@
 #include <Servo.h>
+
 Servo meuServo;
+
 int angulo = 0;
+
 #include <CapacitiveSensor.h>
 
+ 
+
 /*
- * CapitiveSense Library Demo Sketch
- * Paul Badger 2008
- * Uses a high value resistor e.g. 10M between send pin and receive pin
- * Resistor effects sensitivity, experiment with values, 50K - 50M. Larger resistor values yield larger sensor values.
- * Receive pin is the sensor pin - try different amounts of foil/metal on this pin
- */
 
+* CapacitiveSense Library Demo Sketch
 
-CapacitiveSensor   cs_4_2 = CapacitiveSensor(4,2);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
-//CapacitiveSensor   cs_4_6 = CapacitiveSensor(4,6);        // 10M resistor between pins 4 & 6, pin 6 is sensor pin, add a wire and or foil
-//CapacitiveSensor   cs_4_8 = CapacitiveSensor(4,8);        // 10M resistor between pins 4 & 8, pin 8 is sensor pin, add a wire and or foil
+* Paul Badger 2008
+
+* Uses a high value resistor e.g. 10M between send pin and receive pin
+
+* Resistor effects sensitivity, experiment with values, 50K - 50M. Larger resistor values yield larger sensor values.
+
+* Receive pin is the sensor pin - try different amounts of foil/metal on this pin
+
+*/
+
+ 
+
+ 
+
+CapacitiveSensor   cs_4_2 = CapacitiveSensor(4,2);        // resistor de 10M entre os pinos 4 & 2; o pino 2 é o do sensor, o qual pode ser conectado ao alumínio
+
+//CapacitiveSensor   cs_4_6 = CapacitiveSensor(4,6);        // resistor de 10M entre os pinos 4 & 6; o pino 6 é o do sensor, o qual pode ser conectado ao alumínio
+
+//CapacitiveSensor   cs_4_8 = CapacitiveSensor(4,8);        // resistor de 10M entre os pinos 4 & 8; o pino 8 é o do sensor, o qual pode ser conectado ao alumínio
+
+ 
 
 void setup()                    
+
 {
+
   meuServo.attach(9);
+
   meuServo.write(angulo);
-   cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+
+   cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // o canal não se auto calibra
+
+ 
+
    Serial.begin(9600);
-   pinMode(13, OUTPUT);
+
+   //pinMode(9, OUTPUT);
+
 }
 
+ 
+
 void loop()                    
+
 {
-  
+
+   
+
     long start = millis();
+
     long total1 =  cs_4_2.capacitiveSensor(30);
+
   //  long total2 =  cs_4_6.capacitiveSensor(30);
+
   //  long total3 =  cs_4_8.capacitiveSensor(30);
 
-    Serial.print(millis() - start);        // check on performance in milliseconds
+ 
+
+    Serial.print(millis() - start);        // configuração para milissegundos
+
     Serial.print("\t");                    // tab character for debug windown spacing
 
-    Serial.println(total1);                  // print sensor output 1
-    Serial.print("\t");
-//    Serial.print(total2);                  // print sensor output 2
-    Serial.print("\t");
-//    Serial.println(total3);                // print sensor output 3
+ 
 
-if (total1 > 5000){
-  meuServo.write(180); 
+    Serial.println(total1);                  // output 1
+
+    Serial.print("\t");
+
+//    Serial.print(total2);                  // output 2
+
+    Serial.print("\t");
+
+//    Serial.println(total3);                // output 3
+
+ 
+
+if (total1 > 50){
+  Serial.println("180180180180180");
+
+  meuServo.write(180);  
+
   } else {
+
     meuServo.write(0);
+
   }  }
-  
